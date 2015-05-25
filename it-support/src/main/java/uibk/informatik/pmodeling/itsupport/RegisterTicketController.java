652b7299dff71f67661d781701c21933b92400f5
timmornYE <t.yellow.eyes@gmail.com>
2015-05-25 18:29:20 +0200

    test

--- it-support/src/main/java/uibk/informatik/pmodeling/itsupport/RegisterTicketController.java
+++ it-support/src/main/java/uibk/informatik/pmodeling/itsupport/RegisterTicketController.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
  
 @Named
 @ConversationScoped
-public class RegisterTicketController implements Serializable {
+public class RegisterTicketController implements Serializable { 
 	
 	private static  final long serialVersionUID = 1L;
 	 
